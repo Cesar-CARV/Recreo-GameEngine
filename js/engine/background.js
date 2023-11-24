be45ed35f12e51da1d44f709573d4654f99bf90a
cesar-CARV <cesaruizvera@gmail.com>
2023-11-23 19:34:47 -0600

    Se modifico la clase background para arreglar un error al dibujar una imagen 🤷‍

--- js/engine/background.js
+++ js/engine/background.js
@@ -1,14 +1,15 @@
 import Obj from "./object.js";
 
 export default class Background extends Obj{
-    constructor(game, x, y, w, h, image = undefined, color = undefined){
+    constructor(game, x, y, w, h, imageURL = undefined, color = undefined){
         super(game, x, y, w, h);
-        this.image = image;
+        this.img = new Image();
+        this.img.src = imageURL;
         this.color = color;
     }
 
     draw = (ctx) => {
-        if (this.image) ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
+        if (this.image) ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
         if (this.color){
             ctx.fillStyle = this.color;
             ctx.fillRect(this.x, this.y, this.w, this.h);
