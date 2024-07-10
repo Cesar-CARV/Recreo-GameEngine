884d8658fab4534fbbdcbc5ba3c9b451114b4fd7
Cesar-CARV <cesaruizvera@gmail.com>
2024-07-10 15:47:36 -0600

    docs: se documento la clase Input

--- GameEngine/Input.js
+++ GameEngine/Input.js
@@ -1,59 +1,113 @@
 export default class Input {
+  /**
+   * 
+   * @param {string} key 
+   * @returns
+   */
   static GetKeyDown = (key) => {
     let down = this.keydown.has(key);
     this.keydown.delete(key);
     return down;
   };
 
+  /**
+   * 
+   * @param {string} key 
+   * @returns 
+   */
   static GetKeyPress = (key) => {
     return this.keypress.has(key);
   };
 
+  /**
+   * 
+   * @param {string} key 
+   * @returns 
+   */
   static GetKeyUp = (key) => {
     let up = this.keyup.has(key);
     this.keyup.delete(key);
     return up;
   };
+  /**
+   * 
+   * @param {number} button 
+   * @returns 
+   */
   static GetMouseDown = (button) => {
     let down = this.mousedown.has(button);
     this.mousedown.delete(button);
     return down;
   };
 
+  /**
+   * 
+   * @param {number} button 
+   * @returns 
+   */
   static GetMousePress = (button) => {
     return this.mousepress.has(button);
   };
 
+  /**
+   * 
+   * @param {number} button 
+   * @returns 
+   */
   static GetMouseUp = (button) => {
     let up = this.mouseup.has(button);
     this.mouseup.delete(button);
     return up;
   };
 
+  /**
+   * 
+   * @returns 
+   */
   static GetMouseCords = () => {
     return this.mouseCord;
   };
 
+  /**
+   * 
+   * @param {KeyboardEvent} e 
+   */
   static _OnKeyDown = (e) => {
     this.keydown.add(e.key);
     this.keypress.add(e.key);
     this.keyup.delete(e.key);
   };
+  /**
+   * 
+   * @param {KeyboardEvent} e 
+   */
   static _OnKeyUp = (e) => {
     this.keyup.add(e.key);
     this.keydown.delete(e.key);
     if (this.keypress.has(e.key)) this.keypress.delete(e.key);
   };
+  /**
+   * 
+   * @param {MouseEvent} e 
+   */
   static _OnMouseDown = (e) => {
     this.mousedown.add(e.button);
     this.mousepress.add(e.button);
     this.mouseup.delete(e.button);
   };
+  /**
+   * 
+   * @param {MouseEvent} e 
+   */
   static _OnMouseUp = (e) => {
     this.mouseup.add(e.button);
     this.mousedown.delete(e.button);
     if (this.mousepress.has(e.button)) this.mousepress.delete(e.button);
   };
+  /**
+   * 
+   * @param {MouseEvent} e 
+   */
   static _OnMouseMove = (e) => {
     const scaleX = this._GAME.viewport.x / this._GAME.$.clientWidth;
     const scaleY = this._GAME.viewport.y / this._GAME.$.clientHeight;
@@ -64,6 +118,12 @@ export default class Input {
     };
   };
 
+  /**
+   * 
+   * @param {object} GAME 
+   * @param {HTMLCanvasElement} target 
+   * @returns 
+   */
   static Init(GAME, target) {
     if (!target) return;
 
