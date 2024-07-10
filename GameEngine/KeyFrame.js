3605b3d67609f7d5b87e84681209893007bbd9d7
Cesar-CARV <cesaruizvera@gmail.com>
2024-07-10 16:08:24 -0600

    docs: se documento la clase KeyFrame

--- GameEngine/KeyFrame.js
+++ GameEngine/KeyFrame.js
@@ -1,4 +1,13 @@
 export default class KeyFrame {
+  /**
+   * 
+   * @param {number} cutX 
+   * @param {number} cutY 
+   * @param {number} cutW 
+   * @param {number} cutH 
+   * @param {number} tileWidth 
+   * @param {number} tileHeight 
+   */
   constructor(cutX, cutY, cutW, cutH, tileWidth, tileHeight) {
     this.cutX = cutX;
     this.cutY = cutY;
