363afbd812b8081bae7b60d023b991e776fe24bd
Cesar-CARV <cesaruizvera@gmail.com>
2024-07-10 16:15:16 -0600

    docs: se documento la clase Object

--- GameEngine/Object.js
+++ GameEngine/Object.js
@@ -2,6 +2,14 @@ import Vector2 from "./Vector2.js";
 
 export default class Object {
   #created = false;
+  /**
+   * 
+   * @param {object} GAME 
+   * @param {number} x 
+   * @param {number} y 
+   * @param {number} w 
+   * @param {number} h 
+   */
   constructor(GAME, x, y, w, h) {
     this._GAME = GAME;
     this._NAME = undefined;
@@ -29,6 +37,11 @@ export default class Object {
   };
 
   // actualizar poscion relativa
+  /**
+   * 
+   * @param {number} x 
+   * @param {number} y 
+   */
   changePosition = (x, y) => {
     this.position = this._PARENT
       ? new Vector2(this._PARENT.position.x + x, this._PARENT.position.y + y)
@@ -56,6 +69,11 @@ export default class Object {
   };
 
   // agrega un hijo al array de _CHILDREN y le agrega un nombre
+  /**
+   * 
+   * @param {Object} obj 
+   * @param {string} name 
+   */
   addChild = (obj, name) => {
     if (!this._CHILDREN[name]) {
       this._CHILDREN[name] = obj;
@@ -70,6 +88,11 @@ export default class Object {
   };
 
   // retorna un hijo segun el nombre
+  /**
+   * 
+   * @param {string} name 
+   * @returns 
+   */
   getChild = (name) => {
     return this._CHILDREN[name];
     // const child = this._CHILDREN.filter((ch) => ch.name === name)[0];
@@ -77,6 +100,11 @@ export default class Object {
   };
 
   // elimina un hijo segun el nombre
+  /**
+   * 
+   * @param {string} name 
+   * @returns 
+   */
   delteChild = (name) => {
     return delete this._CHILDREN[name];
     // this._CHILDREN = this._CHILDREN.filter((ch) => ch.name !== name);
@@ -85,6 +113,10 @@ export default class Object {
   // dentro de esta funcion se podra utilizar el ContextGraphic del canvas para dibujar cualquier cosa
   // como recomendacion solo utilizarla para dibujar ya que si se afecta al ContextGraphic de cierto modo
   // esto podria afectar a el resto de el motor
+  /**
+   * 
+   * @param {CanvasRenderingContext2D} ctx 
+   */
   draw = (ctx) => {};
 
   // en este evento es donde se programaran las acciones del objeto,
@@ -95,6 +127,10 @@ export default class Object {
   // al ser renderizado
   // NOTA: si se llega a modificar esta funcion procurar seguir el mismo orden y de preferencia no borrar
   // los metodos originales.
+  /**
+   * 
+   * @param {CanvasRenderingContext2D} ctx 
+   */
   main = (ctx) => {
     if (!this.#created) {
       this.onCreate();
