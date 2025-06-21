67f98d0aa62f1d31cb73c8a0c0ec51f31d350cb1
Cesar-CARV <cesaruizvera@gmail.com>
2025-06-20 22:35:13 -0600

    feat: Added tocuh event to input class

--- GameEngine/Input.js
+++ GameEngine/Input.js
@@ -17,12 +17,20 @@ export default class Input {
     this.mouseup = new Set();
     this.mouseCord = { x: 0, y: 0 };
 
+    this.screenTouchesDown = {};
+    this.screenTouchesUp = {};
+    this.screenTouches = {};
+
     target.addEventListener("keydown", (e) => this._OnKeyDown(e));
     target.addEventListener("keyup", (e) => this._OnKeyUp(e));
     target.addEventListener("mousedown", (e) => this._OnMouseDown(e));
     target.addEventListener("mouseup", (e) => this._OnMouseUp(e));
     target.addEventListener("mousemove", (e) => this._OnMouseMove(e));
     target.addEventListener("contextmenu", (e) => e.preventDefault());
+    target.addEventListener("touchstart", (e) => this._OnTouchStart(e));
+    target.addEventListener("touchend", (e) => this._OnTouchEnd(e));
+    target.addEventListener("touchmove", (e) => this._OnTouchMove(e));
+
     target.addEventListener("blur", (e) => {
       this.keydown.clear();
       this.keypress.clear();
@@ -30,6 +38,9 @@ export default class Input {
       this.mousedown.clear();
       this.mousepress.clear();
       this.mouseup.clear();
+      this.screenTouches = {};
+      this.screenTouchesDown = {};
+      this.screenTouchesUp = {};
     });
   }
 
@@ -38,8 +49,11 @@ export default class Input {
     this.mouseup.clear();
     this.keydown.clear();
     this.mousedown.clear();
+    this.screenTouchesDown = {};
+    this.screenTouchesUp = {};
   };
 
+  // ? KEYBOARD
   /**
    *
    * @param {string} key
@@ -69,6 +83,8 @@ export default class Input {
     let up = this.keyup.has(key);
     return up;
   };
+
+  // ? MOUSE
   /**
    *
    * @param {number} button
@@ -107,6 +123,89 @@ export default class Input {
     return this.mouseCord;
   };
 
+  // ? TOUCH
+  GetTouches = () => Object.values(this.screenTouches);
+
+  GetTouchesDown = () => Object.values(this.screenTouchesDown);
+
+  /**
+   *
+   * @param {object} touches
+   * @param {number} x
+   * @param {number} y
+   * @param {number} x2
+   * @param {number} y2
+   */
+  #_filterTouchOnArea = (touches, x, y, x2, y2) => {
+    let target = null;
+    for (let i = 0; i < touches.length; i++) {
+      if (
+        touches[i].x + touches[i].radius >= x &&
+        touches[i].x - touches[i].radius <= x2 &&
+        touches[i].y + touches[i].radius >= y &&
+        touches[i].y - touches[i].radius <= y2
+      ) {
+        target = touches[i];
+        break;
+      }
+    }
+
+    return target;
+  };
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} x2
+   * @param {number} y2
+   */
+  GetTouchOnArea = (x, y, x2, y2) => {
+    return this.#_filterTouchOnArea(
+      Object.values(this.screenTouches),
+      x,
+      y,
+      x2,
+      y2
+    );
+  };
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} x2
+   * @param {number} y2
+   */
+  GetTouchDownOnArea = (x, y, x2, y2) => {
+    return this.#_filterTouchOnArea(
+      Object.values(this.screenTouchesDown),
+      x,
+      y,
+      x2,
+      y2
+    );
+  };
+
+  /**
+   *
+   * @param {number} x
+   * @param {number} y
+   * @param {number} x2
+   * @param {number} y2
+   */
+  GetTouchUpOnArea = (x, y, x2, y2) => {
+    return this.#_filterTouchOnArea(
+      Object.values(this.screenTouchesUp),
+      x,
+      y,
+      x2,
+      y2
+    );
+  };
+
+  // ? Local Methods
+  // ? KEYBOARD
   /**
    *
    * @param {KeyboardEvent} e
@@ -116,6 +215,7 @@ export default class Input {
     this.keypress.add(e.key);
     this.keyup.delete(e.key);
   };
+
   /**
    *
    * @param {KeyboardEvent} e
@@ -125,6 +225,8 @@ export default class Input {
     this.keydown.delete(e.key);
     if (this.keypress.has(e.key)) this.keypress.delete(e.key);
   };
+
+  // ? MOUSE
   /**
    *
    * @param {MouseEvent} e
@@ -155,5 +257,66 @@ export default class Input {
       x: e.offsetX * scaleX,
       y: e.offsetY * scaleY,
     };
+  };
+
+  // ? TOUCH
+  /**
+   *
+   * @param {e: TouchEvent} e
+   */
+  _OnTouchStart = (e) => {
+    e.preventDefault();
+    const scaleX = this._GAME.viewport.x / this._GAME.$.clientWidth;
+    const scaleY = this._GAME.viewport.y / this._GAME.$.clientHeight;
+
+    [...e.changedTouches].forEach((touch) => {
+      this.screenTouches[touch.identifier] = {
+        id: touch.identifier,
+        x: (touch.clientX - e.target.getBoundingClientRect().left) * scaleX,
+        y: (touch.clientY - e.target.getBoundingClientRect().top) * scaleY,
+        radius: Math.floor(touch.radiusX),
+      };
+    });
+
+    this.screenTouchesDown = this.screenTouches;
+  };
+
+  /**
+   *
+   * @param {e: TouchEvent} e
+   */
+  _OnTouchEnd = (e) => {
+    e.preventDefault();
+    const scaleX = this._GAME.viewport.x / this._GAME.$.clientWidth;
+    const scaleY = this._GAME.viewport.y / this._GAME.$.clientHeight;
+
+    [...e.changedTouches].forEach((touch) => {
+      this.screenTouchesUp[touch.identifier] = this.screenTouches[
+        touch.identifier
+      ] = {
+        id: touch.identifier,
+        x: (touch.clientX - e.target.getBoundingClientRect().left) * scaleX,
+        y: (touch.clientY - e.target.getBoundingClientRect().top) * scaleY,
+        radius: Math.floor(touch.radiusX),
+      };
+      delete this.screenTouches[touch.identifier];
+    });
+  };
+
+  /**
+   *
+   * @param {e: TouchEvent} e
+   */
+  _OnTouchMove = (e) => {
+    e.preventDefault();
+    const scaleX = this._GAME.viewport.x / this._GAME.$.clientWidth;
+    const scaleY = this._GAME.viewport.y / this._GAME.$.clientHeight;
+
+    [...e.changedTouches].forEach((touch) => {
+      this.screenTouches[touch.identifier].x =
+        (touch.clientX - e.target.getBoundingClientRect().left) * scaleX;
+      this.screenTouches[touch.identifier].y =
+        (touch.clientY - e.target.getBoundingClientRect().top) * scaleY;
+    });
   };
 }
